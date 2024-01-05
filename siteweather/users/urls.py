faa7d2955d9a1e3969c135f22ac73113ec0c6688
Victor Smirnov <csatom@yandex.ru>
2024-01-05 17:03:50 +0300

    ready forgot password change with e-mail

--- siteweather/users/urls.py
+++ siteweather/users/urls.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.views import LogoutView, PasswordChangeDoneView
-from django.urls import path
+from django.contrib.auth.views import LogoutView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import path, reverse_lazy
 from . import views
 
 app_name = 'users'
@@ -14,5 +15,25 @@ urlpatterns = [
          view=PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
          name='password_change_done'
     ),
-
+    path(route='password-reset/',
+         view=PasswordResetView.as_view(template_name='users/password_reset_form.html',
+                                        email_template_name='users/password_reset_email.html',
+                                        success_url=reverse_lazy('users:password_reset_done')
+                                        ),
+         name='password_reset'
+         ),
+    path(route='password-reset/done/',
+         view=PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'
+         ),
+    path(route='password-reset/<uidb64>/<token>/',
+         view=PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                                success_url=reverse_lazy('users:password_reset_complete')
+                                               ),
+         name='password_reset_confirm'
+         ),
+    path(route='password-reset/complete/',
+         view=PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'
+         ),
 ]
