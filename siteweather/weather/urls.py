eaf8fe24e4dfe16ff1ab3a80c0b92194097e3c42
Victor Smirnov <csatom@yandex.ru>
2024-01-09 18:35:18 +0300

    added search template page

--- siteweather/weather/urls.py
+++ siteweather/weather/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path(route='', view=views.index, name='home'),
     path(route='about/', view=views.about, name='about'),
+    path(route='search/', view=views.search, name='search'),
 
 ]
--- siteweather/weather/views.py
+++ siteweather/weather/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
+
+
 def index(request):
     data = {'title': 'Главная страница'}
     return render(request=request, template_name='weather/index.html', context=data)
@@ -10,6 +12,14 @@ def index(request):
 def about(request):
     data = {'title': 'О проекте'}
     return render(request=request, template_name='weather/about.html', context=data)
+
+
+def search(request):
+    form_data = request.POST
+    if 'city_name' in form_data:
+        city_name = form_data['city_name']
+        data = {'title': 'Результаты поиска', 'city_name': city_name}
+        return render(request=request, template_name='weather/search.html', context=data)
 
 
 def page_not_found(request, exception):
